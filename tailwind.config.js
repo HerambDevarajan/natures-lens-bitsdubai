b5c189e95cc41f2640fd2de6066298248d2cd14e
HerambDevarajan <103058598+HerambDevarajan@users.noreply.github.com>
2024-09-04 11:49:06 +0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'rajdhani': ['Rajdhani', 'sans-serif'],
+        'bitter': ['Bitter', 'serif'],
+      },
+      colors: {
+        'primary-bg': '#013328',
+        'secondary-bg': '#1a564b',
+      },
+    },
+  },
+  plugins: [],
+}
